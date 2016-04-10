218e966892ef107d7ec8c4ac694f5df075968ff7
boltar <tony.cha@gmail.com>
2016-04-10 20:52:49 +0000

    reorder regex processing to give priority to commands

--- clint.js
+++ clint.js
@@ -102,45 +102,6 @@ bot.api.users.list({}, function(err, response) {
   }
 })
 
-controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-  bot.api.reactions.add({
-    timestamp: message.ts,
-    channel: message.channel,
-    name: 'robot_face',
-  }, function(err, res) {
-    if (err) {
-      bot.botkit.log("Failed to add emoji reaction :(", err);
-    }
-  });
-
-
-  controller.storage.users.get(message.user, function(err, user) {
-    if (user && user.name) {
-      bot.reply(message, "Hello " + user.name + "!!");
-    }
-    else {
-      bot.reply(message, "Hello.");
-    }
-  });
-})
-
-controller.hears(['call me (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
-  var matches = message.text.match(/call me (.*)/i);
-  var name = matches[1];
-  controller.storage.users.get(message.user, function(err, user) {
-    if (!user) {
-      user = {
-        id: message.user,
-      }
-    }
-    user.name = name;
-    controller.storage.users.save(user, function(err, id) {
-      bot.reply(message, "Got it. I will call you " + user.name + " from now on.");
-    })
-  })
-});
-
 
 controller.hears(['(\\w+)\.(regint|intreg|reg_int|int_reg) ([\\w:]+)'], 'direct_message,direct_mention,mention,ambient', function(bot, message) {
   console.log("heard regint")
@@ -280,52 +241,6 @@ controller.hears(['help'], 'direct_message,direct_mention,mention', function(bot
     
     bot.reply(message, help_text)
 });
-
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention,ambient', function(bot, message) {
-
-  controller.storage.users.get(message.user, function(err, user) {
-    if (user && user.name) {
-      bot.reply(message, "Your name is: " + user.name);
-    }
-    else {
-      bot.reply(message, "I don't know yet!");
-    }
-  })
-});
-
-
-controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-  bot.startConversation(message, function(err, convo) {
-    convo.ask("Are you sure you want me to shutdown?", [{
-      pattern: bot.utterances.yes,
-      callback: function(response, convo) {
-        convo.say("Bye!");
-        convo.next();
-        setTimeout(function() {
-          process.exit();
-        }, 3000);
-      }
-    }, {
-      pattern: bot.utterances.no,
-      default: true,
-      callback: function(response, convo) {
-        convo.say("*Phew!*");
-        convo.next();
-      }
-    }])
-  })
-})
-
-
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-  var hostname = os.hostname();
-  var uptime = formatUptime(process.uptime());
-
-  bot.reply(message, ':robot_face: I am a bot named <@' + bot.identity.name + '>. I have been running for ' + uptime + ' on ' + hostname + ".");
-
-})
 
 function formatUptime(uptime) {
   var unit = 'second';
@@ -599,5 +514,90 @@ controller.hears(['(\\w+)\.del_list ([\\w:]+)'], 'direct_message,direct_mention,
       bot.reply(message, "No lists found");
     }
   })
+});
+
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+  bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'robot_face',
+  }, function(err, res) {
+    if (err) {
+      bot.botkit.log("Failed to add emoji reaction :(", err);
+    }
+  });
+
+
+  controller.storage.users.get(message.user, function(err, user) {
+    if (user && user.name) {
+      bot.reply(message, "Hello " + user.name + "!!");
+    }
+    else {
+      bot.reply(message, "Hello.");
+    }
+  });
+});
+
+controller.hears(['call me (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
+  var matches = message.text.match(/call me (.*)/i);
+  var name = matches[1];
+  controller.storage.users.get(message.user, function(err, user) {
+    if (!user) {
+      user = {
+        id: message.user,
+      }
+    }
+    user.name = name;
+    controller.storage.users.save(user, function(err, id) {
+      bot.reply(message, "Got it. I will call you " + user.name + " from now on.");
+    })
+  })
+});
+
+controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention,ambient', function(bot, message) {
+
+  controller.storage.users.get(message.user, function(err, user) {
+    if (user && user.name) {
+      bot.reply(message, "Your name is: " + user.name);
+    }
+    else {
+      bot.reply(message, "I don't know yet!");
+    }
+  })
+});
+
+
+controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+  bot.startConversation(message, function(err, convo) {
+    convo.ask("Are you sure you want me to shutdown?", [{
+      pattern: bot.utterances.yes,
+      callback: function(response, convo) {
+        convo.say("Bye!");
+        convo.next();
+        setTimeout(function() {
+          process.exit();
+        }, 3000);
+      }
+    }, {
+      pattern: bot.utterances.no,
+      default: true,
+      callback: function(response, convo) {
+        convo.say("*Phew!*");
+        convo.next();
+      }
+    }])
+  })
+});
+
+
+controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+  var hostname = os.hostname();
+  var uptime = formatUptime(process.uptime());
+
+  bot.reply(message, ':robot_face: I am a bot named <@' + bot.identity.name + '>. I have been running for ' + uptime + ' on ' + hostname + ".");
+
 });
 
